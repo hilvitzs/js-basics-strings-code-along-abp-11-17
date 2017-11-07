44f9ae6d3d4e57b4ca898ff860ade9f1393ec538
Spencer Hilvitz <spencer.hilvitz@gmail.com>
2017-11-07 20:45:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 let excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-let shortGreeting = `Welcome, ${currentUser.slice(0)}`;
+let shortGreeting = `Welcome, ${currentUser.slice(1, 0)}!`;
